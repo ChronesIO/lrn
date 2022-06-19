21e86bbebb7e638083db1e050d960daa42d44d9c
CXCubeHD <cxcubehd@gmail.com>
2022-06-19 18:56:07 +0200

    updated element-layout-base.cc

--- src/lrn-impl/element/element-layout-base.cc
+++ src/lrn-impl/element/element-layout-base.cc
@@ -2,7 +2,7 @@
 
 #include <chr>
 
-#include <lrn-impl/layout/core/inc>
+#include <lrn-impl/layout/instance/inc>
 
 #include "def"
 #include "./element-i.cc"
@@ -18,7 +18,40 @@ namespace lrn
         ~ElementLayoutBase();
 
     private:
+        union
+        {
+            lrn::LayoutInstance_ LayoutInstance_;
+            lrn::LayoutInstance_ LI_;
+        };
+
+    private:
+        chr::list<Element*> LayoutContent_;
+
+    public:
+        CHR_PROP(get = GetLayoutContent)
+        chr::list<Element*> LayoutContent;
+        CHR_ND auto GetLayoutContent() const -> const chr::list<Element*>& { return LayoutContent_; }
+
+    private:
         virtual auto LayoutAdd(Element* e) -> void;
         virtual auto LayoutRemove(Element* e) -> void;
+
+    protected:
+        // bool RaiseOnLayoutChanged = false;
+        // bool RaiseOnSizeChanged = false;
+        // bool RaiseOnMinSizeChanged = false;
+        // bool RaiseOnMaxSizeChanged = false;
+        // bool RaiseOnMarginChanged = false;
+        // bool RaiseOnPaddingChanged = false;
+        // bool RaiseOnAspectRatioChanged = false;
+
+        bool RaiseOnComputedLayoutChanged = false;
+        bool RaiseOnComputedSizeChanged = false;
+        bool RaiseOnComputedPositionChanged = false;
+
+    protected:
+        virtual auto OnComputedLayoutChanged() -> void { }
+        virtual auto OnComputedSizeChanged() -> void { }
+        virtual auto OnComputedPositionChanged() -> void { }
     };
 }
