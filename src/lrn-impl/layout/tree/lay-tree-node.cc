daf50b3aadb172ce552bedd82a673930bd19e7cd
CXCubeHD <cxcubehd@gmail.com>
2022-06-19 18:55:43 +0200

    added LayoutTree (will maybe be used sometime)

---
+++ src/lrn-impl/layout/tree/lay-tree-node.cc
@@ -0,0 +1,12 @@
+#pragma once
+
+//#include <lrn-impl/layout/core/inc>
+//
+//namespace lrn
+//{
+//    struct LayoutTreeNode_
+//    {
+//        chr::vec<LayoutTreeNode_*> Lower;
+//        LayoutTreeNode_* Upper;
+//    };
+//}
---
+++ src/lrn-impl/layout/tree/lay-tree.cc
@@ -0,0 +1,11 @@
+#pragma once
+
+//#include "./lay-tree-node.cc"
+//
+//namespace lrn
+//{
+//    struct LayoutTree_
+//    {
+//
+//    };
+//}
